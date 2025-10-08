d0c86d2caa27aed6838a1b7a69b503ec113092e9
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-08 09:44:47 +0100

    Link specialist table with requests table

--- src/main/java/com/medicale/consultation/consultationmedicale/models/person/Specialist.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/models/person/Specialist.java
@@ -2,9 +2,12 @@ package com.medicale.consultation.consultationmedicale.models.person;
 
 import com.medicale.consultation.consultationmedicale.enums.Gender;
 import com.medicale.consultation.consultationmedicale.enums.Speciality;
+import com.medicale.consultation.consultationmedicale.models.consultation.Request;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "specialists")
@@ -18,6 +21,9 @@ public class Specialist extends Person{
 
     @Column(nullable = false, precision = 10, scale = 2)
     private double fee;
+
+    @OneToMany(mappedBy = "specialist", cascade = CascadeType.ALL)
+    private List<Request> requests = new ArrayList<>();
 
     public Specialist(int id, String firstName, String lastName, String username, String password, String phone, Gender gender, LocalDate createdAt, String numeroRPPS, Speciality speciality, double fee) {
         super(id, firstName, lastName, username, password, phone, gender, createdAt);
