a16c8f7074589341fc0966d9b8016dc81e5a1cbc
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-10 18:42:58 +0100

    Add patient service and repository to implement search and creating for new one into database

--- src/main/java/com/medicale/consultation/consultationmedicale/repositories/PatientRepository.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/repositories/PatientRepository.java
@@ -2,6 +2,7 @@ package com.medicale.consultation.consultationmedicale.repositories;
 
 import com.medicale.consultation.consultationmedicale.models.person.Patient;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 
 import java.util.List;
@@ -23,8 +24,21 @@ public class PatientRepository {
         return em.find(Patient.class, id);
     }
 
+    public Patient findByFirstAndLastName(String firstname, String lastname) {
+        try {
+            TypedQuery<Patient> query = em.createQuery("SELECT p FROM Patient p WHERE p.firstName = :firstname AND p.lastName = :lastname", Patient.class);
+            query.setParameter("firstname", firstname);
+            query.setParameter("lastname", lastname);
+
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
     public List<Patient> findAll() {
-        TypedQuery<Patient> query = em.createQuery("SELECT p FROM Patient p", Patient.class);;
+        TypedQuery<Patient> query = em.createQuery("SELECT p FROM Patient p", Patient.class);
+        ;
         return query.getResultList();
     }
 }
