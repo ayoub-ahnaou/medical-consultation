8c303be43b3887e7c6163e3868dfd929262d81de
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-10 18:44:09 +0100

    Add patientService with a function to find patient by thier first and last names

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/services/user/PatientService.java
@@ -0,0 +1,39 @@
+package com.medicale.consultation.consultationmedicale.services.user;
+
+import com.medicale.consultation.consultationmedicale.enums.Gender;
+import com.medicale.consultation.consultationmedicale.enums.Role;
+import com.medicale.consultation.consultationmedicale.models.person.Patient;
+import com.medicale.consultation.consultationmedicale.repositories.PatientRepository;
+import jakarta.persistence.EntityManager;
+
+import java.time.LocalDate;
+
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(EntityManager em) {
+        this.patientRepository = new PatientRepository(em);
+    }
+
+    public Patient findByFirstAndLastName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            return null;
+        }
+
+        String[] parts = name.trim().split("\\s+", 2);
+        String firstname = parts[0];
+        String lastname = parts.length > 1 ? parts[1] : "";
+
+        return patientRepository.findByFirstAndLastName(firstname, lastname);
+    }
+
+    public Patient findById(int id) {
+        return patientRepository.findById(id);
+    }
+
+    public Patient create(String firstName, String lastName, String username, String password, String phone, Gender gender, Role role, String dossierNumber, LocalDate dateOfBirth, double height, double weight) {
+        Patient patient = new Patient(firstName, lastName, username, password, phone, gender, role, dossierNumber, dateOfBirth, height, weight);
+        patientRepository.save(patient);
+        return patient;
+    }
+}
