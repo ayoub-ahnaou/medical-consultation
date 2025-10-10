ebb88cc8be508ee1f028f7f35635dd16d1449b17
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-10 18:36:14 +0100

    Fix seeding users record after changing person constructor (created at field)

--- src/main/java/com/medicale/consultation/consultationmedicale/utils/Seeder.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/utils/Seeder.java
@@ -18,22 +18,22 @@ public class Seeder {
         try {
             tx.begin();
 
-            em.persist(new Nurse(0, "Alice", "Smith", "nurse1", PasswordUtils.hashPassword("1234"),"0612345678", Gender.FEMALE, LocalDate.now(), Role.NURSE));
-            em.persist(new Nurse(0, "Bob", "Brown", "nurse2", PasswordUtils.hashPassword("1234"),"0612345679", Gender.MALE, LocalDate.now(), Role.NURSE));
-            em.persist(new Nurse(0, "Carol", "Johnson", "nurse3", PasswordUtils.hashPassword("1234"),"0612345680", Gender.FEMALE, LocalDate.now(), Role.NURSE));
-            em.persist(new Nurse(0, "Diana", "King", "nurse4", PasswordUtils.hashPassword("1234"), "0612345681", Gender.FEMALE, LocalDate.now(), Role.NURSE));
-            em.persist(new Nurse(0, "Ethan", "Wright", "nurse5", PasswordUtils.hashPassword("1234"), "0612345682", Gender.MALE, LocalDate.now(), Role.NURSE));
+            em.persist(new Nurse("Alice", "Smith", "nurse1", PasswordUtils.hashPassword("1234"),"0612345678", Gender.FEMALE, Role.NURSE));
+            em.persist(new Nurse("Bob", "Brown", "nurse2", PasswordUtils.hashPassword("1234"),"0612345679", Gender.MALE, Role.NURSE));
+            em.persist(new Nurse("Carol", "Johnson", "nurse3", PasswordUtils.hashPassword("1234"),"0612345680", Gender.FEMALE, Role.NURSE));
+            em.persist(new Nurse("Diana", "King", "nurse4", PasswordUtils.hashPassword("1234"), "0612345681", Gender.FEMALE, Role.NURSE));
+            em.persist(new Nurse("Ethan", "Wright", "nurse5", PasswordUtils.hashPassword("1234"), "0612345682", Gender.MALE, Role.NURSE));
 
-            em.persist(new Generalist(0, "John", "Doe", "generalist1", PasswordUtils.hashPassword("1234"), "0623456780", Gender.MALE, LocalDate.now(), Role.GENERALIST, "RPPS001", "12 Main St", "Casablanca", 300.0));
-            em.persist(new Generalist(0, "Emma", "White", "generalist2", PasswordUtils.hashPassword("1234"), "0623456781", Gender.FEMALE, LocalDate.now(), Role.GENERALIST, "RPPS002", "45 Elm St", "Rabat", 280.0));
-            em.persist(new Generalist(0, "Liam", "Scott", "generalist3", PasswordUtils.hashPassword("1234"), "0623456783", Gender.MALE, LocalDate.now(), Role.GENERALIST, "RPPS003", "12 Boulevard", "Fes", 280.0));
-            em.persist(new Generalist(0, "Sophia", "Adams", "generalist4", PasswordUtils.hashPassword("1234"), "0623456784", Gender.FEMALE, LocalDate.now(), Role.GENERALIST, "RPPS004", "98 Street", "Marrakech", 320.0));
+            em.persist(new Generalist("John", "Doe", "generalist1", PasswordUtils.hashPassword("1234"), "0623456780", Gender.MALE, Role.GENERALIST, "RPPS001", "12 Main St", "Casablanca", 300.0));
+            em.persist(new Generalist("Emma", "White", "generalist2", PasswordUtils.hashPassword("1234"), "0623456781", Gender.FEMALE, Role.GENERALIST, "RPPS002", "45 Elm St", "Rabat", 280.0));
+            em.persist(new Generalist("Liam", "Scott", "generalist3", PasswordUtils.hashPassword("1234"), "0623456783", Gender.MALE, Role.GENERALIST, "RPPS003", "12 Boulevard", "Fes", 280.0));
+            em.persist(new Generalist("Sophia", "Adams", "generalist4", PasswordUtils.hashPassword("1234"), "0623456784", Gender.FEMALE, Role.GENERALIST, "RPPS004", "98 Street", "Marrakech", 320.0));
 
-            em.persist(new Specialist(0, "Michael", "Lee", "specialist1", PasswordUtils.hashPassword("1234"), "0634567890", Gender.MALE, LocalDate.now(), Role.SPECIALIST, "RPPS101", Speciality.CARDIOLOGY, 500.0));
-            em.persist(new Specialist(0, "Olivia", "Davis", "specialist2", PasswordUtils.hashPassword("1234"), "0634567891", Gender.FEMALE, LocalDate.now(), Role.SPECIALIST, "RPPS102", Speciality.DERMATOLOGY, 450.0));
-            em.persist(new Specialist(0, "William", "Taylor", "specialist3", PasswordUtils.hashPassword("1234"), "0634567892", Gender.MALE, LocalDate.now(), Role.SPECIALIST, "RPPS103", Speciality.NEUROLOGY, 550.0));
-            em.persist(new Specialist(0, "Isabella", "Martinez", "specialist4", PasswordUtils.hashPassword("1234"), "0634567893", Gender.FEMALE, LocalDate.now(), Role.SPECIALIST, "RPPS104", Speciality.FAMILY, 550.0));
-            em.persist(new Specialist(0, "Alexander", "Harris", "specialist5", PasswordUtils.hashPassword("1234"), "0634567894", Gender.MALE, LocalDate.now(), Role.SPECIALIST, "RPPS105", Speciality.NEUROLOGY, 450.0));
+            em.persist(new Specialist("Michael", "Lee", "specialist1", PasswordUtils.hashPassword("1234"), "0634567890", Gender.MALE, Role.SPECIALIST, "RPPS101", Speciality.CARDIOLOGY, 500.0));
+            em.persist(new Specialist("Olivia", "Davis", "specialist2", PasswordUtils.hashPassword("1234"), "0634567891", Gender.FEMALE, Role.SPECIALIST, "RPPS102", Speciality.DERMATOLOGY, 450.0));
+            em.persist(new Specialist("William", "Taylor", "specialist3", PasswordUtils.hashPassword("1234"), "0634567892", Gender.MALE, Role.SPECIALIST, "RPPS103", Speciality.NEUROLOGY, 550.0));
+            em.persist(new Specialist("Isabella", "Martinez", "specialist4", PasswordUtils.hashPassword("1234"), "0634567893", Gender.FEMALE, Role.SPECIALIST, "RPPS104", Speciality.FAMILY, 550.0));
+            em.persist(new Specialist("Alexander", "Harris", "specialist5", PasswordUtils.hashPassword("1234"), "0634567894", Gender.MALE, Role.SPECIALIST, "RPPS105", Speciality.NEUROLOGY, 450.0));
 
             tx.commit();
             System.out.println("Users seeded successfully...");
