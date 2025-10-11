80a2cee8f126a9cfed0b4a63764b03606f92587f
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-11 12:51:29 +0100

    Caa for update medical file if it exist and create a ticket in both cases of medical file existance

--- src/main/java/com/medicale/consultation/consultationmedicale/servlets/patient/AddMedicaleFileServlet.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/servlets/patient/AddMedicaleFileServlet.java
@@ -1,11 +1,13 @@
-package com.medicale.consultation.consultationmedicale.servlets;
+package com.medicale.consultation.consultationmedicale.servlets.patient;
 
 import com.medicale.consultation.consultationmedicale.config.JPAUtils;
 import com.medicale.consultation.consultationmedicale.models.MedicaleFile;
 import com.medicale.consultation.consultationmedicale.models.person.Patient;
 import com.medicale.consultation.consultationmedicale.repositories.MedicaleFileRepository;
 import com.medicale.consultation.consultationmedicale.services.MedicaleFileService;
+import com.medicale.consultation.consultationmedicale.services.TicketService;
 import com.medicale.consultation.consultationmedicale.services.user.PatientService;
+import com.medicale.consultation.consultationmedicale.servlets.BaseServlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,14 +20,14 @@ public class AddMedicaleFileServlet extends BaseServlet {
 
     private MedicaleFileService medicaleFileService;
     private PatientService patientService;
+    private TicketService ticketService;
 
     @Override
     public void init() throws ServletException {
         super.init();
         this.medicaleFileService = new MedicaleFileService(new MedicaleFileRepository(JPAUtils.getEntityManager()));
         this.patientService = new PatientService(JPAUtils.getEntityManager());
-
-        System.out.println("AddMedicaleFileServlet loaded");
+        this.ticketService = new TicketService(JPAUtils.getEntityManager());
     }
 
     @Override
@@ -44,19 +46,35 @@ public class AddMedicaleFileServlet extends BaseServlet {
         try {
             int patientId = Integer.parseInt(req.getParameter("patientId"));
             Patient patient = patientService.findById(patientId);
+            // check if medical file already exists for this patient
+            MedicaleFile medicaleFile = medicaleFileService.findByPatientId(patientId);
 
-            MedicaleFile file = new MedicaleFile();
-            file.setPatient(patient);
-            file.setTemperature(Double.parseDouble(req.getParameter("temperature")));
-            file.setPulse(Integer.parseInt(req.getParameter("pulse")));
-            file.setBloodPresure(Integer.parseInt(req.getParameter("bloodPresure")));
-            file.setRespiratoryRate(Integer.parseInt(req.getParameter("respiratoryRate")));
-            file.setOxygenSaturation(Double.parseDouble(req.getParameter("oxygenSaturation")));
-            file.setPain(Integer.parseInt(req.getParameter("pain")));
+            if(medicaleFile != null){
+                medicaleFile.setTemperature(Double.parseDouble(req.getParameter("temperature")));
+                medicaleFile.setPulse(Integer.parseInt(req.getParameter("pulse")));
+                medicaleFile.setBloodPresure(Integer.parseInt(req.getParameter("bloodPresure")));
+                medicaleFile.setRespiratoryRate(Integer.parseInt(req.getParameter("respiratoryRate")));
+                medicaleFile.setOxygenSaturation(Double.parseDouble(req.getParameter("oxygenSaturation")));
+                medicaleFile.setPain(Integer.parseInt(req.getParameter("pain")));
 
-            medicaleFileService.create(file);
+                medicaleFileService.update(medicaleFile);
+                req.setAttribute("success", "Dossier médical mis à jour avec succès !");
+            } else {
+                MedicaleFile file = new MedicaleFile();
+                file.setPatient(patient);
+                file.setTemperature(Double.parseDouble(req.getParameter("temperature")));
+                file.setPulse(Integer.parseInt(req.getParameter("pulse")));
+                file.setBloodPresure(Integer.parseInt(req.getParameter("bloodPresure")));
+                file.setRespiratoryRate(Integer.parseInt(req.getParameter("respiratoryRate")));
+                file.setOxygenSaturation(Double.parseDouble(req.getParameter("oxygenSaturation")));
+                file.setPain(Integer.parseInt(req.getParameter("pain")));
 
-            req.setAttribute("success", "Dossier médical ajouté avec succès !");
+                medicaleFileService.create(file);
+                req.setAttribute("success", "Dossier médical ajouté avec succès !");
+            }
+            // create a ticket for current patient juste inserted
+            ticketService.createTicketForPatient(patient);
+
             resp.sendRedirect(req.getContextPath() + "/dashboard"); // redirect after saving
         } catch (Exception e) {
             e.printStackTrace();
