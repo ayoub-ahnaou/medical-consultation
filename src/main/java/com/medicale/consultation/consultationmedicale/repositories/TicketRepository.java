ebb8537c2d73871adae95011e3701ba5b020f30d
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-11 12:50:17 +0100

    Create crud functions for ticket model in service and repo layer

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/repositories/TicketRepository.java
@@ -0,0 +1,30 @@
+package com.medicale.consultation.consultationmedicale.repositories;
+
+import com.medicale.consultation.consultationmedicale.models.Ticket;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class TicketRepository {
+    private EntityManager em;
+
+    public TicketRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Ticket findById(int id) {
+        return em.find(Ticket.class, id);
+    }
+
+    public void save(Ticket ticket) {
+        em.getTransaction().begin();
+        em.persist(ticket);
+        em.getTransaction().commit();
+    }
+
+    public List<Ticket> findAll() {
+        TypedQuery<Ticket> query = em.createQuery("SELECT t FROM Ticket t", Ticket.class);
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/com/medicale/consultation/consultationmedicale/services/TicketService.java
@@ -0,0 +1,31 @@
+package com.medicale.consultation.consultationmedicale.services;
+
+import com.medicale.consultation.consultationmedicale.models.Ticket;
+import com.medicale.consultation.consultationmedicale.models.person.Patient;
+import com.medicale.consultation.consultationmedicale.repositories.TicketRepository;
+import jakarta.persistence.EntityManager;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    public TicketService(EntityManager em) {
+        this.ticketRepository = new TicketRepository(em);
+    }
+
+    public List<Ticket> findAll() {
+        return ticketRepository.findAll();
+    }
+
+    public void create(Ticket ticket) {
+        ticketRepository.save(ticket);
+    }
+
+    public void createTicketForPatient(Patient patient) {
+        Ticket ticket = new Ticket(patient, LocalDateTime.now());
+        ticketRepository.save(ticket);
+    }
+}
