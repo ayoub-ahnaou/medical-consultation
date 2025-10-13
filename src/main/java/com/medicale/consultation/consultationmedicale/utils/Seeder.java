a682943955e4c2bafa9786493866915886acd911
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-14 00:26:37 +0100

    Seed medical acts direclty also after running application

--- src/main/java/com/medicale/consultation/consultationmedicale/utils/Seeder.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/utils/Seeder.java
@@ -3,6 +3,7 @@ package com.medicale.consultation.consultationmedicale.utils;
 import com.medicale.consultation.consultationmedicale.enums.Gender;
 import com.medicale.consultation.consultationmedicale.enums.Role;
 import com.medicale.consultation.consultationmedicale.enums.Speciality;
+import com.medicale.consultation.consultationmedicale.models.consultation.MedicaleAct;
 import com.medicale.consultation.consultationmedicale.models.person.Generalist;
 import com.medicale.consultation.consultationmedicale.models.person.Nurse;
 import com.medicale.consultation.consultationmedicale.models.person.Specialist;
@@ -35,8 +36,30 @@ public class Seeder {
             em.persist(new Specialist("Isabella", "Martinez", "specialist4", PasswordUtils.hashPassword("1234"), "0634567893", Gender.FEMALE, Role.SPECIALIST, "RPPS104", Speciality.FAMILY, 550.0));
             em.persist(new Specialist("Alexander", "Harris", "specialist5", PasswordUtils.hashPassword("1234"), "0634567894", Gender.MALE, Role.SPECIALIST, "RPPS105", Speciality.NEUROLOGY, 450.0));
 
+            // persist medical acts also into database
+            em.persist(new MedicaleAct("General Consultation", 50.0));
+            em.persist(new MedicaleAct("Specialist Consultation", 80.0));
+            em.persist(new MedicaleAct("Blood Test - Complete Blood Count", 25.0));
+            em.persist(new MedicaleAct("X-Ray - Chest", 45.0));
+            em.persist(new MedicaleAct("MRI Scan - Brain", 300.0));
+            em.persist(new MedicaleAct("CT Scan - Abdomen", 250.0));
+            em.persist(new MedicaleAct("Ultrasound - Abdominal", 120.0));
+            em.persist(new MedicaleAct("Vaccination - Influenza", 30.0));
+            em.persist(new MedicaleAct("Minor Surgery - Suturing", 150.0));
+            em.persist(new MedicaleAct("Dental Checkup and Cleaning", 60.0));
+            em.persist(new MedicaleAct("Eye Examination - Comprehensive", 55.0));
+            em.persist(new MedicaleAct("Physical Therapy Session", 75.0));
+            em.persist(new MedicaleAct("ECG - Electrocardiogram", 40.0));
+            em.persist(new MedicaleAct("Urine Analysis", 15.0));
+            em.persist(new MedicaleAct("Allergy Testing - Skin Prick", 90.0));
+            em.persist(new MedicaleAct("Skin Biopsy", 110.0));
+            em.persist(new MedicaleAct("Colonoscopy", 400.0));
+            em.persist(new MedicaleAct("Endoscopy - Upper GI", 350.0));
+            em.persist(new MedicaleAct("Echocardiogram", 200.0));
+            em.persist(new MedicaleAct("Bone Density Test", 85.0));
+
             tx.commit();
-            System.out.println("Users seeded successfully...");
+            System.out.println("Records seeded successfully...");
         } catch (Exception e) {
             if (tx.isActive()) tx.rollback();
             e.printStackTrace();
