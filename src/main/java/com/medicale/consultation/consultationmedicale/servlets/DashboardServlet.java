d8d8bbe96990e31944b9c54ca46577b15eab796a
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-14 00:41:18 +0100

    Redirect user with needed attributs to thier dashboard based on role

--- src/main/java/com/medicale/consultation/consultationmedicale/servlets/DashboardServlet.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/servlets/DashboardServlet.java
@@ -1,6 +1,11 @@
 package com.medicale.consultation.consultationmedicale.servlets;
 
+import com.medicale.consultation.consultationmedicale.config.JPAUtils;
 import com.medicale.consultation.consultationmedicale.enums.Role;
+import com.medicale.consultation.consultationmedicale.models.Ticket;
+import com.medicale.consultation.consultationmedicale.models.person.Patient;
+import com.medicale.consultation.consultationmedicale.services.TicketService;
+import com.medicale.consultation.consultationmedicale.services.user.PatientService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,9 +13,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "dashboardServlet", value = "/dashboard")
 public class DashboardServlet extends BaseServlet{
+    private TicketService  ticketService;
+    private PatientService patientService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.ticketService = new TicketService(JPAUtils.getEntityManager());
+        this.patientService = new PatientService(JPAUtils.getEntityManager());
+    }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,8 +39,16 @@ public class DashboardServlet extends BaseServlet{
         String role = session.getAttribute("userRole").toString();
 
         switch (role) {
-            case "NURSE" -> req.getRequestDispatcher("/WEB-INF/views/nurse/dashboard.jsp").forward(req, resp);
-            case "GENERALIST" -> req.getRequestDispatcher("/WEB-INF/views/generalist/dashboard.jsp").forward(req, resp);
+            case "NURSE" -> {
+                List<Patient> patients = patientService.findAll();
+                req.setAttribute("patients", patients);
+                req.getRequestDispatcher("/WEB-INF/views/nurse/dashboard.jsp").forward(req, resp);
+            }
+            case "GENERALIST" -> {
+                List<Ticket> tickets = ticketService.findAllByDateDesc();
+                req.setAttribute("tickets", tickets);
+                req.getRequestDispatcher("/WEB-INF/views/generalist/dashboard.jsp").forward(req, resp);
+            }
             case "SPECIALIST" -> req.getRequestDispatcher("/WEB-INF/views/specialist/dashboard.jsp").forward(req, resp);
             default -> resp.sendError(HttpServletResponse.SC_FORBIDDEN);
         }
