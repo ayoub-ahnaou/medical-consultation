7386a72901764fbf2008193107f92ab05da8ec3b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-10-14 00:23:48 +0100

    Add medical file attribut to patient model

--- src/main/java/com/medicale/consultation/consultationmedicale/models/person/Patient.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/models/person/Patient.java
@@ -2,6 +2,7 @@ package com.medicale.consultation.consultationmedicale.models.person;
 
 import com.medicale.consultation.consultationmedicale.enums.Gender;
 import com.medicale.consultation.consultationmedicale.enums.Role;
+import com.medicale.consultation.consultationmedicale.models.MedicaleFile;
 import com.medicale.consultation.consultationmedicale.models.Ticket;
 import jakarta.persistence.*;
 
@@ -26,6 +27,9 @@ public class Patient extends Person {
 
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
     private List<Ticket> tickets = new ArrayList<>();
+
+    @OneToOne(mappedBy = "patient", cascade = CascadeType.ALL)
+    private MedicaleFile medicaleFile;
 
     public Patient(String firstName, String lastName, String username, String password, String phone, Gender gender, Role role, String dossierNumber, LocalDate dateOfBirth, double height, double weight) {
         super(firstName, lastName, username, password, phone, gender, role);
@@ -69,5 +73,21 @@ public class Patient extends Person {
 
     public void setWeight(double weight) {
         this.weight = weight;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public MedicaleFile getMedicaleFile() {
+        return medicaleFile;
+    }
+
+    public void setMedicaleFile(MedicaleFile medicaleFile) {
+        this.medicaleFile = medicaleFile;
     }
 }
